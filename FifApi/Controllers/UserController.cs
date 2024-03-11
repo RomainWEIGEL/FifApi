da21c2c20b9d573055533281154707a19547d3ae
canalea <canalea@D360-S23>
2024-03-11 16:55:42 +0100

    ajot de models lgin + user

---
+++ FifApi/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using FifApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TP_Token.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetUserData")]
+        [Authorize(Policy = Policies.User)]
+        public IActionResult GetUserData()
+        {
+            return Ok("This is a response from user method");
+        }
+        [HttpGet]
+        [Route("GetAdminData")]
+        [Authorize(Policy = Policies.Admin)]
+        public IActionResult GetAdminData()
+        {
+            return Ok("This is a response from Admin method");
+        }
+    }
+}
