1f5c2728accc7bbef6c4530b5d729e17b2e8d462
canalea <canalea@D360-S23>
2024-03-12 16:43:03 +0100

    ajout CouleurProduit

---
+++ FifApi/Models/EntityFramework/CouleurProduit.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FifApi.Models.EntityFramework
+{
+    public class CouleurProduit
+    {
+        private double prix;
+        private string codeBarre;
+
+        public CouleurProduit( double prix, string codeBarre )
+        {
+            this.prix = prix;
+            this.codeBarre = codeBarre;
+        }
+
+        [Key]
+        [Column("prix", TypeName = "decimal(8,2)")]
+        public double Prix
+        {
+            get { return prix; }
+            set { prix = value; }
+        }
+
+        [Column("codebarre", TypeName = "varchar(48)")]
+        public string CodeBarre
+        {
+            get { return codeBarre; }
+            set { codeBarre = value; }
+        }
+    }
+}
