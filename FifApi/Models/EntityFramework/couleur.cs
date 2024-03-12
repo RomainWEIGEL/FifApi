96928db06ca6a93b336b6bc78ba165c7ff2fb509
canalea <canalea@D360-S23>
2024-03-12 16:13:09 +0100

    ajout couleur

---
+++ FifApi/Models/EntityFramework/couleur.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FifApi.Models.EntityFramework
+{
+    [Table("couleur")]
+    public class Couleur
+    {
+        private int idCouleur;
+        private string nomCouleur;
+
+        public Couleur(int idCouleur, string nomCouleur)
+        {
+            this.idCouleur = idCouleur;
+            this.nomCouleur = nomCouleur;
+        }
+
+        [Key]
+        [Column("idcouleur", TypeName = "int")]
+        public int IdCouleur
+        {
+            get { return idCouleur; }
+            set { idCouleur = value; }
+        }
+
+        [Column("nomproduit", TypeName = "varchar(150)")]
+        public string NomCouleur
+        {
+            get { return nomCouleur; }
+            set { nomCouleur = value; }
+        }
+
+    }
+}
